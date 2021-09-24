95a46c4b951752438b91349826242dc38a649de7
Alexander <81567588+sashacozub@users.noreply.github.com>
2021-09-24 14:17:35 +0200

    Add modal

--- src/components/ResultsList/ResultsList.js
+++ src/components/ResultsList/ResultsList.js
@@ -2,7 +2,20 @@ import React, { useState } from 'react';
 import './ResultsList.css';
 
 const ResultsList = (props) => {
-  const [isModalOn, setIsModalOn] = useState(true);
+  const [isModalOn, setIsModalOn] = useState(false);
+  const [selectedCocktail, setSelectedCocktail] = useState({});
+
+  const handleModalOpening = (e) => {
+    const clickedCocktail = props.results.filter((result) => {
+      return result.name === e.target.alt;
+    });
+    setSelectedCocktail(clickedCocktail[0]);
+    setIsModalOn(true);
+  };
+
+  const handleModalClosing = () => {
+    setIsModalOn(false);
+  };
 
   if (props.isLoading) {
     return (
@@ -20,14 +33,44 @@ const ResultsList = (props) => {
           return (
             <li key={result.id}>
               <h3>{result.name}</h3>
-              <img src={result.photo} alt={result.name} />
+              <img
+                src={result.photo}
+                alt={result.name}
+                onClick={handleModalOpening}
+              />
             </li>
           );
         })}
       </ul>
+
       {isModalOn && (
         <div className='modal'>
-          <h1>MODAL</h1>
+          <div className='cocktail-info'>
+            <button className='close-modal-btn' onClick={handleModalClosing}>
+              X
+            </button>
+            <h2>{selectedCocktail.name}</h2>
+            <img src={selectedCocktail.photo} alt={selectedCocktail.name} />
+            <div className='cocktail-description'>
+              <p>
+                <strong>Glass:</strong> {selectedCocktail.glass}
+              </p>
+              <p>
+                <strong>Procedure:</strong> {selectedCocktail.procedure}
+              </p>
+              <p>
+                <strong>Ingredients:</strong>
+              </p>
+              <p>{selectedCocktail.ingredients.ingredient_1}</p>
+              <p>{selectedCocktail.ingredients.ingredient_2}</p>
+              <p>{selectedCocktail.ingredients.ingredient_3}</p>
+              <p>{selectedCocktail.ingredients.ingredient_4}</p>
+              <p>{selectedCocktail.ingredients.ingredient_5}</p>
+              <p>{selectedCocktail.ingredients.ingredient_6}</p>
+              <p>{selectedCocktail.ingredients.ingredient_7}</p>
+              <p>{selectedCocktail.ingredients.ingredient_8}</p>
+            </div>
+          </div>
         </div>
       )}
     </section>
--- src/utils/Cocktails.js
+++ src/utils/Cocktails.js
@@ -12,12 +12,23 @@ const Cocktails = {
           return [];
         }
         return jsonResponse.drinks.map((drink) => {
+          console.log(jsonResponse);
           return {
             id: drink.idDrink,
             photo: drink.strDrinkThumb,
             name: drink.strDrink,
             glass: drink.strGlass,
             procedure: drink.strInstructions,
+            ingredients: {
+              ingredient_1: drink.strIngredient1,
+              ingredient_2: drink.strIngredient2,
+              ingredient_3: drink.strIngredient3,
+              ingredient_4: drink.strIngredient4,
+              ingredient_5: drink.strIngredient5,
+              ingredient_6: drink.strIngredient6,
+              ingredient_7: drink.strIngredient7,
+              ingredient_8: drink.strIngredient8,
+            },
           };
         });
       }
